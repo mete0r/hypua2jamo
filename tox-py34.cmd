d1e73a65e677f6c2fc661d903815547182984057
mete0r <mete0r@sarangbang.or.kr>
2018-12-29 23:20:44 +0900

    appveyor.yml experiments

--- tox-py34.cmd
+++ tox-py34.cmd
@@ -1,6 +1,2 @@
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /x86
-cd c:\projects\hypua2jamo
-c:\python27\scripts\virtualenv .
-call scripts\activate
-pip install -U setuptools pip tox
 tox -e py34
