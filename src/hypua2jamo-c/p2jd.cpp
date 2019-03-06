1e85ca29ed1f79d3d16a576ad39aa4e9e291a9ff
mete0r <mete0r@sarangbang.or.kr>
2019-03-07 01:26:01 +0900

    Workaround for MSVC compilers

--- src/hypua2jamo-c/p2jd.cpp
+++ src/hypua2jamo-c/p2jd.cpp
@@ -13,35 +13,77 @@ typedef unsigned int uint32_t;
 
 
 #include "p2jd-table.h"
-#include "p2jx.hpp"
+
+
+template <typename codepoint_t>
+inline int p2jd_calcsize(const codepoint_t *src, int srclen) {
+	const unsigned short *jamo_seq;
+	int ret = 0;
+	const codepoint_t *src_end = src + srclen;
+	for (; src < src_end; ++src) {
+		jamo_seq = lookup(*src);
+		if (jamo_seq != NULL) {
+			ret += jamo_seq[0];
+		} else {
+			ret += 1;
+		}
+	}
+	return ret;
+}
+
+
+template <typename codepoint_t>
+inline int p2jd_encode(const codepoint_t *src, int srclen, codepoint_t *dst) {
+	const unsigned short *jamo_seq;
+	int jamo_len;
+	const codepoint_t *src_end = src + srclen;
+	const codepoint_t *dst_start = dst;
+	for (; src < src_end; ++src) {
+		jamo_seq = lookup(*src);
+		if (jamo_seq == NULL) {
+			*(dst++) = *src;
+		} else {
+			jamo_len = *(jamo_seq++);
+			while (jamo_len-- > 0) {
+				*(dst++) = *(jamo_seq++);
+			}
+		}
+	}
+	return dst - dst_start;
+}
 
 
 extern "C" int hypua_p2jd_ucs2_calcsize(const uint16_t *src, int srclen) {
-	return calcsize(src, srclen);
+	return p2jd_calcsize(src, srclen);
 }
 
 
 extern "C" int hypua_p2jd_ucs2_encode(const uint16_t *src, int srclen, uint16_t *dst) {
-	return encode(src, srclen, dst);
+	return p2jd_encode(src, srclen, dst);
 }
 
 
 extern "C" int hypua_p2jd_ucs4_calcsize(const uint32_t *src, int srclen) {
-	return calcsize(src, srclen);
+	return p2jd_calcsize(src, srclen);
 }
 
 
 extern "C" int hypua_p2jd_ucs4_encode(const uint32_t *src, int srclen, uint32_t *dst) {
-	return encode(src, srclen, dst);
+	return p2jd_encode(src, srclen, dst);
 }
 
 
-extern "C" void hypua_p2jd_ucs2_test() {
+extern "C" void hypua_p2jd_ucs2_debug() {
 	uint16_t src = 0xF7CA;
-	int size = hypua_p2jd_ucs2_calcsize(&src, 1);
-	void *buf = malloc(size);
-	int n_encoded = hypua_p2jd_ucs2_encode(&src, 1, (uint16_t*)buf);
-	printf("hypua_p2jd_ucs2_test: size=%d encoded=%d\n", size, n_encoded);
-	printf("p2jd_F7CA: len=%d\n", p2jd_F7CA[0]);
+
+	printf("p2jd_F7CA: 0x%p len=%d\n", p2jd_F7CA, p2jd_F7CA[0]);
+	const uint16_t *p2jd = lookup(src);
+	printf("p2jdfound: 0x%p len=%d\n", p2jd, p2jd[0]);
+
+	int buf_size = p2jd_calcsize(&src, 1);
+	printf("p2jd_calcsize: size=%d\n", buf_size);
+	uint16_t *buf = (uint16_t *) malloc(buf_size * sizeof(uint16_t));
+	int n_encoded = p2jd_encode(&src, 1, (uint16_t*)buf);
 	free(buf);
+	printf("p2jd_encode: encoded=%d\n", n_encoded);
 }
