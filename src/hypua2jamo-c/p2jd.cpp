8969edfdba6780f97393eb28d671169b0680fdf9
mete0r <mete0r@sarangbang.or.kr>
2019-03-20 02:36:59 +0900

    p2jd.cpp: remove debug function (for windows build)

--- src/hypua2jamo-c/p2jd.cpp
+++ src/hypua2jamo-c/p2jd.cpp
@@ -71,19 +71,3 @@ extern "C" int hypua_p2jd_ucs4_calcsize(const uint32_t *src, int srclen) {
 extern "C" int hypua_p2jd_ucs4_encode(const uint32_t *src, int srclen, uint32_t *dst) {
 	return p2jd_encode(src, srclen, dst);
 }
-
-
-extern "C" void hypua_p2jd_ucs2_debug() {
-	uint16_t src = 0xF7CA;
-
-	printf("p2jd_F7CA: 0x%p len=%d\n", p2jd_F7CA, p2jd_F7CA[0]);
-	const uint16_t *p2jd = lookup(src);
-	printf("p2jdfound: 0x%p len=%d\n", p2jd, p2jd[0]);
-
-	int buf_size = p2jd_calcsize(&src, 1);
-	printf("p2jd_calcsize: size=%d\n", buf_size);
-	uint16_t *buf = (uint16_t *) malloc(buf_size * sizeof(uint16_t));
-	int n_encoded = p2jd_encode(&src, 1, (uint16_t*)buf);
-	free(buf);
-	printf("p2jd_encode: encoded=%d\n", n_encoded);
-}
