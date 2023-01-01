b101fbb463a86d09d1b807b049dd9f65a4f85205
Yoosung Moon <arbeitmachtfrei77@gmail.com>
2023-01-01 21:04:36 +0900

    appveyor: 윈도우 빌드 개선
    - 윈도우 환경에서 amd64 바이너리 빌드 추가 (Fix #23)
    - 윈도우 환경에서 Python 3.9, 3.10, 3.11 용 바이너리 빌드 (Fix #16)
    - 윈도우 환경에서 Python 2.7 용 바이너리 빌드 복원
    - 컴파일러 환경 설정으로 vcvarsall.bar 사용
    - Python 3.9 이상에서 테스트 시 pytest가 실패하고 있어서 unittest
      discover로 변경

--- ccompiler-vs2019.cmd
+++ ccompiler-vs2019.cmd
@@ -1,2 +1,2 @@
 REM https://learn.microsoft.com/ko-kr/cpp/build/building-on-the-command-line?view=msvc-170#use-the-developer-tools-in-an-existing-command-window
-call "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" %vcarch%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %vcarch%
--- ccompiler-winsdk71.cmd
+++
@@ -1,1 +0,0 @@
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /x86
